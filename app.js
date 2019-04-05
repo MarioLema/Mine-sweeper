36deb34d8dcbf6d99ab0982dff8d4b48544962be
Mario Lema <lema_mario@yahoo.com>
2019-04-05 23:26:48 +0100

    add styling

--- app.js
+++ app.js
@@ -6,42 +6,49 @@ const DATA = {
     revealed: [],
     cols: 10,
     rows: 10,
-    mineRate: 1.5,
+    mineRate: 1.7,
     resolution: 50,
-    width(){return this.resolution * this.cols},
-    height(){return this.resolution * this.rows},
-    cells(){return this.cols * this.rows},
+    width() {
+        return this.resolution * this.cols
+    },
+    height() {
+        return this.resolution * this.rows
+    },
+    cells() {
+        return this.cols * this.rows
+    },
 
 };
 
 //===========================================VIEW METHODS========================================================
 const VIEW = {
 
-    generateBoard(){
+    generateBoard() {
         let grid = document.getElementById(`board`);
         grid.setAttribute(`style`, `width: ${DATA.width()}px; height: ${DATA.height()}px; grid-template-columns: repeat(${DATA.cols}, 1fr); grid-template-rows: repeat(${DATA.rows}, 1fr)`);
         let fragment = document.createDocumentFragment();
-        for(let i = 0; i < DATA.boardArr.length; i++){
+        for (let i = 0; i < DATA.boardArr.length; i++) {
             let el = document.createElement('div');
-        el.classList.add(`cell`);
-        el.setAttribute(`id`, `cell-${i}`);
-        fragment.appendChild(el);
+            el.classList.add(`cell`);
+            el.setAttribute(`id`, `cell-${i}`);
+
+            fragment.appendChild(el);
         }
         grid.appendChild(fragment);
     },
 
-    displayMine(id){
+    displayMine(id) {
         let cell = document.getElementById(`cell-${id}`);
         cell.classList.add(`reveal-mine`);
-        cell.innerHTML= `💣`;
+        cell.innerHTML = `💣`;
     },
 
-    displayEmpty(neighbours, id){
+    displayEmpty(neighbours, id) {
         let cell = document.getElementById(`cell-${id}`);
         cell.classList.add(`reveal-empty`);
-        if(neighbours > 0){
+        if (neighbours > 0) {
             cell.innerHTML = neighbours;
-        }else{
+        } else {
             cell.innerHTML = ` `;
         }
     },
@@ -54,44 +61,47 @@ const VIEW = {
 //===========================================MODIFIER METHODS========================================================
 const MODIFIER = {
 
-    populateBoardArr(){
-        for(let i = 0; i < DATA.cells() ; i++){
+    populateBoardArr() {
+        for (let i = 0; i < DATA.cells(); i++) {
             let random = Math.random() * 2;
-            if(random > DATA.mineRate){
+            if (random > DATA.mineRate) {
                 DATA.boardArr.push(1);
                 DATA.mineCell.push(i);
-            }else{
+            } else {
                 DATA.boardArr.push(0);
                 DATA.emptyCell.push(i);
             }
         }
     },
 
-    turn(){
+    turn() {
         let id = event.target.id;
         let index = Number(id.slice(5));
 
-        if(this.checkMine(index)){
-            for(let i = 0; i < DATA.mineCell.length; i++){
+        if (this.checkMine(index)) {
+            for (let i = 0; i < DATA.mineCell.length; i++) {
                 VIEW.displayMine(DATA.mineCell[i]);
             }
 
-        }else{
+        } else {
             this.findNeighbours(index);
         }
     },
 
-    findNeighbours(i){
+    findNeighbours(i) {
         let checked = [];
 
-        function checkCell(index){
+        function checkCell(index) {
             let topBound = 0;
             let bottomBound = DATA.boardArr.length - 1;
-            let leftBound = Math.floor(index/DATA.rows) * DATA.rows ;
-            let rightBound = (Math.ceil(index/DATA.rows) * DATA.rows ) - 1;
-            if(index < topBound || index > bottomBound || index < leftBound || index > rightBound) return; //if the index number is outside of the boundaries of the grid row or column
-            if(checked.indexOf(index) === -1 ) { checked.push(index) } else return; //if it has not been checked before, push it to the array, else return
-            if(DATA.revealed.indexOf(index) !== -1 || DATA.mineCell.indexOf(index) !== -1) return; //if it was revealed previously or it is a mine return
+            let leftBound = Math.floor(index / DATA.rows) * DATA.rows;
+            let rightBound = DATA.rows + (Math.ceil(index / DATA.rows) * DATA.rows) - 1;
+
+            if (index < topBound || index > bottomBound || index < leftBound || index > rightBound) return; //if the index number is outside of the boundaries of the grid row or column
+            if (checked.indexOf(index) === -1) {
+                checked.push(index)
+            } else return; //if it has not been checked before, push it to the array, else return
+            if (DATA.revealed.indexOf(index) !== -1 || DATA.mineCell.indexOf(index) !== -1) return; //if it was revealed previously or it is a mine return
 
 
             let neighbours = [index - 1, index + 1, index - DATA.rows, index - DATA.rows - 1, index - DATA.rows + 1, index + DATA.rows, index + DATA.rows - 1, index + DATA.rows + 1];
@@ -99,29 +109,26 @@ const MODIFIER = {
             DATA.revealed.push(index);
             VIEW.displayEmpty(mineCount, index);
 
-            
-
-            for (let i = 0; i < neighbours.length; i++){
-                checkCell(neighbours[i]);
+            if (mineCount === 0) {
+                for (let i = 0; i < neighbours.length; i++) {
+                    checkCell(neighbours[i]);
+                }
             }
-        }
 
+
+        }
         checkCell(i);
     },
 
-    checkCell(index){
-        
-    },
-
-    mineCount(neighArr){
+    mineCount(neighArr) {
         let count = 0;
-        for(let i = 0; i < neighArr.length; i++){
-            if(DATA.boardArr[neighArr[i]]) count++
+        for (let i = 0; i < neighArr.length; i++) {
+            if (DATA.boardArr[neighArr[i]]) count++
         }
         return count;
     },
 
-    checkMine(index){
+    checkMine(index) {
         return DATA.boardArr[index] ? true : false;
     }
 
@@ -132,18 +139,18 @@ const MODIFIER = {
     //================VALIDATION METHODS==================
     //===================================================
 
-    
+
 
     //================UTILITY METHODS==================
     //===================================================
 
-    
+
 
 
     //================LISTENER METHODS==================
     //===================================================
 
-    
+
 }
 MODIFIER.turn = MODIFIER.turn.bind(MODIFIER);
 // MODIFIER.startGame = MODIFIER.startGame.bind(MODIFIER);
